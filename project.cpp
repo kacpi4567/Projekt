002e5abc15c1cb7d11036b612ac6fbec79c8fef7
kacpi4567 <kacperbudzynski7280@gmail.com>
2025-11-16 17:37:11 +0100

    przeniesienie grafow do nowego pliku glownego (projekt.cpp) i usuniecie zbednych plików (.exe i pliki wykonywalne macOS) oraz dodanie ich do .gitignore

--- project.cpp
+++ project.cpp
@@ -128,19 +128,99 @@ public:
 };
 
 int main() {
-  // GRAPH 1
-  Graph g1;
-  for (int i = 1; i <= 6; i++) {
+  Graph g1, g2, g3;
+  // PIERWSZY GRAF
+  for (int i = 1; i <= 23; i++) {
     g1.addVertex(i);
   }
-  g1.addEdge(1, 2);
-  g1.addEdge(1, 3);
-  g1.addEdge(1, 4);
-  g1.addEdge(3, 4);
-  g1.addEdge(2, 5);
-  g1.addEdge(2, 6);
-  g1.addEdge(3, 6);
-  g1.sortAdjacencyList();
+  for (int i = 1; i < 23; i++) {
+    if (i == 6 || i == 12 || i == 18) {
+      continue;
+    } else {
+      g1.addEdge(i, i + 1);
+    }
+  }
+
+  for (int i = 1; i < 18; i++) {
+    g1.addEdge(i, i + 6);
+  }
+
+  // DRUGI GRAF
+  for (int i = 1; i <= 23; i++) {
+    g2.addVertex(i);
+  }
+  g2.addEdge(1, 5);
+  g2.addEdge(5, 11);
+  g2.addEdge(11, 12);
+  g2.addEdge(12, 13);
+  g2.addEdge(2, 5);
+  g2.addEdge(2, 6);
+  g2.addEdge(6, 11);
+  g2.addEdge(6, 14);
+  g2.addEdge(3, 6);
+  g2.addEdge(3, 7);
+  g2.addEdge(7, 14);
+  g2.addEdge(7, 15);
+  g2.addEdge(15, 16);
+  g2.addEdge(16, 17);
+  g2.addEdge(4, 7);
+  g2.addEdge(4, 8);
+  g2.addEdge(9, 8);
+  g2.addEdge(8, 15);
+  g2.addEdge(8, 19);
+  g2.addEdge(19, 20);
+  g2.addEdge(20, 21);
+  g2.addEdge(5, 6);
+  g2.addEdge(6, 7);
+  g2.addEdge(7, 8);
+  g2.addEdge(9, 10);
+  g2.addEdge(11, 14);
+  g2.addEdge(14, 15);
+  g2.addEdge(15, 19);
+  g2.addEdge(12, 22);
+  g2.addEdge(16, 20);
+  g2.addEdge(20, 23);
+  g2.addEdge(13, 18);
+  g2.addEdge(18, 17);
+  g2.addEdge(17, 21);
+
+  // TRZECI GRAF
+  for (int i = 1; i <= 17; i++) {
+    g3.addVertex(i);
+  }
+  g3.addEdge(13, 14);
+  g3.addEdge(3, 4);
+  g3.addEdge(4, 5);
+  g3.addEdge(5, 10);
+  g3.addEdge(1, 2);
+  g3.addEdge(1, 9);
+  g3.addEdge(6, 10);
+  g3.addEdge(10, 11);
+  g3.addEdge(10, 12);
+  g3.addEdge(9, 8);
+  g3.addEdge(8, 7);
+  g3.addEdge(7, 10);
+  g3.addEdge(16, 15);
+  g3.addEdge(3, 2);
+  g3.addEdge(2, 9);
+  g3.addEdge(9, 17);
+  g3.addEdge(13, 4);
+  g3.addEdge(8, 16);
+  g3.addEdge(14, 5);
+  g3.addEdge(5, 6);
+  g3.addEdge(6, 7);
+  g3.addEdge(7, 15);
+
+  //sortowanie list polaczen grafow
+  g1.sortAdjacencyList(); // SORTOWANIE PRZED WYŚWIETLENIEM
+  g2.sortAdjacencyList(); // SORTOWANIE PRZED WYŚWIETLENIEM
+  g3.sortAdjacencyList(); // SORTOWANIE PRZED WYŚWIETLENIEM
+  //wyswietlenie list polaczen
+  cout<<"Graf 1: \n";
   g1.showAdjacencyList();
+  cout<<"Graf 2: \n";
+  g2.showAdjacencyList();
+  cout<<"Graf 3: \n";
+  g3.showAdjacencyList();
   return 0;
 }
