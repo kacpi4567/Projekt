5824434478febd48a83764e8353b2bf8894f269b
i0i010 <olajablonska2363@gmail.com>
2025-11-16 16:23:56 +0100

    klasa agent i poprawki

--- project.cpp
+++ project.cpp
@@ -1,6 +1,8 @@
 #include <algorithm>
 #include <iostream>
 #include <map>
+#include <memory>
+#include <unordered_set>
 #include <vector>
 using namespace std;
 
@@ -12,13 +14,12 @@ struct Vertex {
 
 class Graph {
 private:
-  typedef map<int, Vertex *> VertexMap;
-  VertexMap vertices;
+  map<int, unique_ptr<Vertex>> vertices;
 
 public:
   void addVertex(int name) {
     if (vertices.find(name) == vertices.end()) {
-      vertices[name] = new Vertex(name);
+      vertices[name] = make_unique<Vertex>(name);
     } else {
       cout << "Vertex " << name << " already exists!\n";
     }
@@ -29,10 +30,8 @@ public:
     } else if (vertices.find(to) == vertices.end()) {
       cout << "Vertex " << to << " does not exist!\n";
     } else {
-      Vertex *f = vertices[from];
-      Vertex *t = vertices[to];
-      f->adj.push_back(to);
-      t->adj.push_back(from);
+      vertices[from]->adj.push_back(to);
+      vertices[to]->adj.push_back(from);
     }
   }
   void sortAdjacencyList() {
@@ -58,29 +57,74 @@ public:
 
 class Agent {
 protected:
-  int startPosition;
+  int position;
+  unordered_set<int> goalPoints;
   int endPosition;
   int currentPosition;
-  int turns = 0;
+  int moves = 0;
   int waits = 0;
   int pardons = 0;
+  string agentType;
+
+public:
+  Agent(int startPosition, unordered_set<int> &goals, int _endPosition,
+        string _agentType)
+      : position{startPosition}, goalPoints{goals}, endPosition{_endPosition},
+        agentType{_agentType} {}
+  bool hasFinished() { return goalPoints.empty() && position == endPosition; }
+  void visitPoint(int point) { goalPoints.erase(point); }
+  void recordMove() { moves++; }
+  void recordWait() { waits++; }
+  void recordPardon() { pardons++; }
+  void show() {
+    cout << agentType << " Agent:\n";
+    cout << "Position: " << position << endl;
+    cout << "Moves: " << moves << endl;
+    cout << "Waits: " << waits << endl;
+    cout << "Pardons: " << pardons << endl;
+    cout << "Goal points not visited: ";
+    if (goalPoints.empty()) {
+      cout << "NONE\n";
+    } else {
+      cout << "{";
+      for (int goal : goalPoints) {
+        if (goalPoints.find(goal) != goalPoints.end()) {
+          cout << ", " << goal;
+        }
+      }
+      cout << "}";
+    }
+    cout << "End position: " << endPosition << endl;
+    cout << "Finished?: " << (hasFinished() ? "YES" : "NO") << endl << endl;
+  }
+  int getPosition() const { return position; }
+  void setPosition(int p) { position = p; }
+  unordered_set<int> &getGoalPoints() { return goalPoints; }
 };
+
 class BFS : public Agent {
 private:
 public:
+  BFS(int _startPosition, unordered_set<int> &goals, int _endPosition)
+      : Agent{_startPosition, goals, _endPosition, "BFS"} {}
 };
 
 class DFS : public Agent {
 private:
 public:
+  DFS(int _startPosition, unordered_set<int> &goals, int _endPosition)
+      : Agent{_startPosition, goals, _endPosition, "DFS"} {}
 };
 
 class Simulation {
 private:
-  Graph g;
+  Graph *g;
+  vector<unique_ptr<Agent>> agents;
+  int currentTurn = 0;
 
 public:
-  Simulation(Graph _g) : g{_g} {}
+  Simulation(Graph *_g) : g{_g} {}
+  void addAgent(unique_ptr<Agent> a) { agents.push_back(move(a)); }
 };
 
 int main() {
